519e9df681bc9f06b73b958600ac7abe53433367
Jose Martin Rozanec <jmrozanec@gmail.com>
2014-08-14 01:15:39 -0300

    Add test for CronFieldExpression

---
+++ src/test/java/com/cron/utils/parser/field/CronFieldExpressionTest.java
@@ -0,0 +1,44 @@
+package com.cron.utils.parser.field;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class CronFieldExpressionTest {
+    private TestCronFieldExpression testCronFieldExpression;
+    private FieldConstraints mockFieldConstraints;
+
+    @Before
+    public void setUp() throws Exception {
+        mockFieldConstraints = mock(FieldConstraints.class);
+        this.testCronFieldExpression = new TestCronFieldExpression(mockFieldConstraints);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullConstraints() throws Exception {
+        new TestCronFieldExpression(null);
+    }
+
+    @Test
+    public void testAnd() throws Exception {
+        CronFieldExpression mockExpression = mock(CronFieldExpression.class);
+        And and = testCronFieldExpression.and(mockExpression);
+        assertTrue(and.getExpressions().contains(mockExpression));
+        assertTrue(and.getExpressions().contains(testCronFieldExpression));
+    }
+
+    @Test
+    public void testGetConstraints() throws Exception {
+        assertEquals(mockFieldConstraints, testCronFieldExpression.getConstraints());
+    }
+
+    class TestCronFieldExpression extends CronFieldExpression {
+
+        public TestCronFieldExpression(FieldConstraints constraints) {
+            super(constraints);
+        }
+    }
+}
