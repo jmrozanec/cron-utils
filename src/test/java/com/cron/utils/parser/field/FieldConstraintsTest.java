f7a195dba90612097798103470216c6e76fd3d4f
Jose Martin Rozanec <jmrozanec@gmail.com>
2014-08-14 11:13:23 -0300

    pom.xml: add Cobertura html report generation

---
+++ src/test/java/com/cron/utils/parser/field/FieldConstraintsTest.java
@@ -0,0 +1,34 @@
+package com.cron.utils.parser.field;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldConstraintsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testStringToInt() throws Exception {
+
+    }
+
+    @Test
+    public void testIntToInt() throws Exception {
+
+    }
+
+    @Test
+    public void testValidateInRange() throws Exception {
+
+    }
+
+    @Test
+    public void testValidateSpecialCharAllowed() throws Exception {
+
+    }
+}
