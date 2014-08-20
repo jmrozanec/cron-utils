c9c9c5f3524783e9a6104c3e9d81954688d80013
Jose Martin Rozanec <jmrozanec@gmail.com>
2014-08-20 08:54:10 -0300

    Add tests for FieldDefinition comparator

--- src/test/java/com/cron/utils/model/FieldDefinitionTest.java
+++ src/test/java/com/cron/utils/model/FieldDefinitionTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class FieldDefinitionTest {
 
@@ -42,5 +43,39 @@ public class FieldDefinitionTest {
     @Test
     public void testGetConstraints() throws Exception {
         assertEquals(mockConstraints, fieldDefinition.getConstraints());
+    }
+
+    @Test
+    public void testCreateFieldDefinitionComparatorNotNull() throws Exception {
+        assertNotNull(FieldDefinition.createFieldDefinitionComparator());
+    }
+
+    @Test
+    public void testCreateFieldDefinitionComparatorEqual() throws Exception {
+        CronFieldName name = CronFieldName.DAY_OF_MONTH;
+        FieldDefinition fieldDefinition1 = new FieldDefinition(name, mockConstraints);
+        FieldDefinition fieldDefinition2 = new FieldDefinition(name, mock(FieldConstraints.class));
+        assertEquals(name.getOrder(), name.getOrder());
+        assertEquals(0, FieldDefinition.createFieldDefinitionComparator().compare(fieldDefinition1, fieldDefinition2));
+    }
+
+    @Test
+    public void testCreateFieldDefinitionComparatorGreater() throws Exception {
+        CronFieldName name1 = CronFieldName.DAY_OF_MONTH;
+        CronFieldName name2 = CronFieldName.SECOND;
+        FieldDefinition fieldDefinition1 = new FieldDefinition(name1, mockConstraints);
+        FieldDefinition fieldDefinition2 = new FieldDefinition(name2, mock(FieldConstraints.class));
+        assertNotEquals(name1.getOrder(), name2.getOrder());
+        assertTrue(FieldDefinition.createFieldDefinitionComparator().compare(fieldDefinition1, fieldDefinition2) > 0);
+    }
+
+    @Test
+    public void testCreateFieldDefinitionComparatorLesser() throws Exception {
+        CronFieldName name1 = CronFieldName.DAY_OF_MONTH;
+        CronFieldName name2 = CronFieldName.SECOND;
+        FieldDefinition fieldDefinition1 = new FieldDefinition(name1, mockConstraints);
+        FieldDefinition fieldDefinition2 = new FieldDefinition(name2, mock(FieldConstraints.class));
+        assertNotEquals(name1.getOrder(), name2.getOrder());
+        assertTrue(FieldDefinition.createFieldDefinitionComparator().compare(fieldDefinition2, fieldDefinition1) < 0);
     }
 }
