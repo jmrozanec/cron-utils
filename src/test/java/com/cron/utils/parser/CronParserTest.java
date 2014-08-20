042f5420e1731f0038b3ae2af1dc69132a826e53
Jose Martin Rozanec <jmrozanec@gmail.com>
2014-08-20 08:53:48 -0300

    Add tests for CronParser

---
+++ src/test/java/com/cron/utils/parser/CronParserTest.java
@@ -0,0 +1,33 @@
+package com.cron.utils.parser;
+
+import com.cron.utils.model.CronDefinition;
+import com.cron.utils.model.FieldDefinition;
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+
+public class CronParserTest {
+    @Mock
+    private CronDefinition definition;
+
+    private CronParser parser;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        Set<FieldDefinition> set = Sets.newHashSet();
+        when(definition.getFieldDefinitions()).thenReturn(set);
+        parser = new CronParser(definition);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParse() throws Exception {
+        parser.parse("");
+    }
+}
