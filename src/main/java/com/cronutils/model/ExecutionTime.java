d7a7b534859930a962ebea97e65fdcb5508f882e
Jose Martin Rozanec <jmrozanec@gmail.com>
2014-09-07 10:37:37 -0300

    Document approaches for ExecutionTime

--- src/main/java/com/cronutils/model/ExecutionTime.java
+++ src/main/java/com/cronutils/model/ExecutionTime.java
@@ -3,16 +3,22 @@ package com.cronutils.model;
 import com.cronutils.model.field.*;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import org.joda.time.DateTime;
 
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+//Approach 1: [Discarded] brute force, iterate over all possible future dates until finding first matching cron.
+//Approach 1: [...] precalculate possible values for each field and take nearest ones
+//Approach 2: [...] nearestValue(FieldExpression, int timeValue): return plus -> method to retrieve value for field an sum
 class ExecutionTime {
 
     private Map<CronFieldName, List<Integer>> executionTimes;
 
     private ExecutionTime(Map<CronFieldName, CronField> fields){
-        this.executionTimes = Maps.newHashMap();
+        this.executionTimes = Collections.unmodifiableMap(fieldsToExecutionTimes(fields));
     }
 
     public static ExecutionTime forCron(Cron cron){
@@ -20,25 +26,28 @@ class ExecutionTime {
     }
 
 //    public DateTime afterDate(DateTime date){
+//        date.plus()
 //    }
 //
 //    public DateTime beforeDate(DateTime date){
 //    }
 
     private List<Integer> fromFieldToTimeValues(FieldExpression fieldExpression, int max){
+        List<Integer> values = Lists.newArrayList();
         if(fieldExpression instanceof And){
-            return fromFieldToTimeValues((And)fieldExpression, max);
+            values = fromFieldToTimeValues((And)fieldExpression, max);
         }
         if(fieldExpression instanceof Between){
-            return fromFieldToTimeValues((Between)fieldExpression, max);
+            values = fromFieldToTimeValues((Between)fieldExpression, max);
         }
         if(fieldExpression instanceof On){
-            return fromFieldToTimeValues((On)fieldExpression, max);
+            values = fromFieldToTimeValues((On)fieldExpression, max);
         }
         if(fieldExpression instanceof Always){
-            return fromFieldToTimeValues((Always)fieldExpression, max);
+            values = fromFieldToTimeValues((Always)fieldExpression, max);
         }
-        return Lists.newArrayList();
+        Collections.sort(values);
+        return values;
     }
 
     private List<Integer> fromFieldToTimeValues(And fieldExpression, int max){
@@ -71,5 +80,37 @@ class ExecutionTime {
             values.add(j);
         }
         return values;
+    }
+
+    private int getMaxForCronField(CronFieldName cronFieldName){
+        switch (cronFieldName){
+            case YEAR:
+                return DateTime.now().getYear() + 60;
+            case MONTH:
+                return 12;
+            case DAY_OF_MONTH:
+                return 31;
+            case DAY_OF_WEEK:
+                return 7;
+            default:
+                return 60;
+        }
+    }
+
+    private Map<CronFieldName, List<Integer>> fieldsToExecutionTimes(Map<CronFieldName, CronField> fields){
+        Map<CronFieldName, List<Integer>> executionTimes = Maps.newHashMap();
+        Iterator<Map.Entry<CronFieldName, CronField>> iterator =
+                fields.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<CronFieldName, CronField> entry = iterator.next();
+            executionTimes.put(
+                    entry.getKey(),
+                    fromFieldToTimeValues(
+                            entry.getValue().getExpression(),
+                            getMaxForCronField(entry.getKey())
+                    )
+            );
+        }
+        return executionTimes;
     }
 }
