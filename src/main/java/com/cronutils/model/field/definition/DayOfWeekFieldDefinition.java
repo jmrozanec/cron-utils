983e0b24892571ecbd3964b5833570378bff349d
Jose Martin Rozanec <jmrozanec@gmail.com>
2015-02-14 10:55:41 -0300

    Add licensing information to files.

--- src/main/java/com/cronutils/model/field/definition/DayOfWeekFieldDefinition.java
+++ src/main/java/com/cronutils/model/field/definition/DayOfWeekFieldDefinition.java
@@ -3,7 +3,18 @@ package com.cronutils.model.field.definition;
 import com.cronutils.mapper.WeekDay;
 import com.cronutils.model.field.CronFieldName;
 import com.cronutils.model.field.constraint.FieldConstraints;
-
+/*
+ * Copyright 2015 jmrozanec
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class DayOfWeekFieldDefinition extends FieldDefinition {
     private WeekDay mondayDoWValue;
 
--- src/test/java/com/cronutils/model/field/definition/FieldDefinitionBuilderTest.java
+++ src/test/java/com/cronutils/model/field/definition/FieldDefinitionBuilderTest.java
@@ -17,7 +17,18 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
-
+/*
+ * Copyright 2015 jmrozanec
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({FieldConstraintsBuilder.class, FieldDefinitionBuilder.class})
 public class FieldDefinitionBuilderTest {
