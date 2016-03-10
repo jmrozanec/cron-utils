02657c1ecf1592695dd9d20781e2445ee88e8f80
Jose Martin Rozanec <jmrozanec@gmail.com>
2016-03-10 10:04:26 -0300

    Add test case for ValueMappingFieldExpressionVisitorTest: validate new QuestionMark instance holds destination field constraints.

---
+++ src/test/java/com/cronutils/model/field/expression/visitor/ValueMappingFieldExpressionVisitorTest.java
@@ -0,0 +1,59 @@
+package com.cronutils.model.field.expression.visitor;
+
+import com.cronutils.model.field.constraint.FieldConstraints;
+import com.cronutils.model.field.constraint.FieldConstraintsBuilder;
+import com.cronutils.model.field.expression.QuestionMark;
+import com.cronutils.model.field.value.FieldValue;
+import com.google.common.base.Function;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValueMappingFieldExpressionVisitorTest {
+    private FieldConstraints destinationConstraints;
+    private ValueMappingFieldExpressionVisitor valueMappingFieldExpressionVisitor;
+
+    @Before
+    public void setUp() throws Exception {
+        destinationConstraints = FieldConstraintsBuilder.instance().createConstraintsInstance();
+        Function<FieldValue, FieldValue> transform = new Function<FieldValue, FieldValue>() {
+            @Override
+            public FieldValue apply(FieldValue input) {
+                return input;
+            }
+        };
+        this.valueMappingFieldExpressionVisitor = new ValueMappingFieldExpressionVisitor(destinationConstraints, transform);
+    }
+
+    @Test
+    public void testVisitAlways() throws Exception {
+
+    }
+
+    @Test
+    public void testVisitAnd() throws Exception {
+
+    }
+
+    @Test
+    public void testVisitBetween() throws Exception {
+
+    }
+
+    @Test
+    public void testVisitEvery() throws Exception {
+
+    }
+
+    @Test
+    public void testVisitOn() throws Exception {
+
+    }
+
+    @Test
+    public void testVisitQuestionMark() throws Exception {
+        QuestionMark questionMark = valueMappingFieldExpressionVisitor.visit(new QuestionMark(FieldConstraintsBuilder.instance().createConstraintsInstance()));
+        assertEquals(destinationConstraints, questionMark.getConstraints());
+    }
+}
