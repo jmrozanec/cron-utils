bd6c07038defa15a9934b7c95850a30fe24b7e32
Łukasz Bąk <lukaszbak01@gmail.com>
2016-06-23 22:41:56 +0200

    Failing unit test for issue #92

--- src/test/java/com/cronutils/model/time/generator/ExecutionTimeUnixIntegrationTest.java
+++ src/test/java/com/cronutils/model/time/generator/ExecutionTimeUnixIntegrationTest.java
@@ -265,4 +265,15 @@ public class ExecutionTimeUnixIntegrationTest {
         ExecutionTime executionTime = ExecutionTime.forCron(cron);
         assertEquals(DateTime.parse("2014-12-01T08:00:00Z"), executionTime.nextExecution(date));
     }
+
+    /**
+     * Issue #92: Next execution skipping valid date
+     */
+    @Test
+    public void testNextExecution2016() {
+        CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.UNIX));
+        ExecutionTime executionTime = ExecutionTime.forCron(parser.parse("1 0 * * tue"));
+        DateTime date = DateTime.parse("2016-05-24T01:02:50Z");
+        assertEquals(DateTime.parse("2016-05-31T00:01:00Z"), executionTime.nextExecution(date));
+    }
 }
