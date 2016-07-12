8ae8cbf223536698bac3f74bf21c1f2a836abe73
albuhuba <albuhuba@yahoo.com>
2016-07-12 18:52:30 +0200

    Java 7 and code readability
    The code quality/readability is now enhanced a little bit. I would like
    to use this tool therefore I would like to contribute to make it better.
    Are you interested in a collaboration?

--- src/main/java/com/cronutils/builder/CronBuilder.java
+++ src/main/java/com/cronutils/builder/CronBuilder.java
@@ -12,6 +12,16 @@
  */
 package com.cronutils.builder;
 
+import static com.cronutils.model.field.CronFieldName.DAY_OF_MONTH;
+import static com.cronutils.model.field.CronFieldName.DAY_OF_WEEK;
+import static com.cronutils.model.field.CronFieldName.HOUR;
+import static com.cronutils.model.field.CronFieldName.MINUTE;
+import static com.cronutils.model.field.CronFieldName.MONTH;
+import static com.cronutils.model.field.CronFieldName.SECOND;
+import static com.cronutils.model.field.CronFieldName.YEAR;
+
+import java.util.Map;
+
 import com.cronutils.model.Cron;
 import com.cronutils.model.definition.CronDefinition;
 import com.cronutils.model.field.CronField;
@@ -19,65 +29,61 @@ import com.cronutils.model.field.CronFieldName;
 import com.cronutils.model.field.constraint.FieldConstraints;
 import com.cronutils.model.field.expression.FieldExpression;
 import com.cronutils.model.field.expression.visitor.ValidationFieldExpressionVisitor;
-
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import java.util.Map;
-
-import static com.cronutils.model.field.expression.FieldExpressionFactory.*;
-
 public class CronBuilder {
-    private CronDefinition definition;
-    private Map<CronFieldName, CronField> fields;
 
-    private CronBuilder(CronDefinition definition){
-        this.definition = definition;
-        this.fields = Maps.newHashMap();
-    }
+	private CronDefinition definition;
+	private Map<CronFieldName, CronField> fields = Maps.newHashMap();
 
-    public static CronBuilder cron(CronDefinition definition){
-        return new CronBuilder(definition);
-    }
+	private CronBuilder(CronDefinition definition) {
+		this.definition = definition;
+	}
 
-    public CronBuilder withYear(FieldExpression expression){
-        return addField(CronFieldName.YEAR, expression);
-    }
+	public static CronBuilder cron(CronDefinition definition) {
+		return new CronBuilder(definition);
+	}
 
-    public CronBuilder withDoM(FieldExpression expression){
-        return addField(CronFieldName.DAY_OF_MONTH, expression);
-    }
+	public CronBuilder withYear(FieldExpression expression) {
+		return addField(YEAR, expression);
+	}
 
-    public CronBuilder withMonth(FieldExpression expression){
-        return addField(CronFieldName.MONTH, expression);
-    }
+	public CronBuilder withDoM(FieldExpression expression) {
+		return addField(DAY_OF_MONTH, expression);
+	}
 
-    public CronBuilder withDoW(FieldExpression expression){
-        return addField(CronFieldName.DAY_OF_WEEK, expression);
-    }
+	public CronBuilder withMonth(FieldExpression expression) {
+		return addField(MONTH, expression);
+	}
 
-    public CronBuilder withHour(FieldExpression expression){
-        return addField(CronFieldName.HOUR, expression);
-    }
+	public CronBuilder withDoW(FieldExpression expression) {
+		return addField(DAY_OF_WEEK, expression);
+	}
 
-    public CronBuilder withMinute(FieldExpression expression){
-        return addField(CronFieldName.MINUTE, expression);
-    }
+	public CronBuilder withHour(FieldExpression expression) {
+		return addField(HOUR, expression);
+	}
 
-    public CronBuilder withSecond(FieldExpression expression){
-        return addField(CronFieldName.SECOND, expression);
-    }
+	public CronBuilder withMinute(FieldExpression expression) {
+		return addField(MINUTE, expression);
+	}
 
-    public Cron instance(){
-        return new Cron(definition, Lists.newArrayList(fields.values())).validate();
-    }
+	public CronBuilder withSecond(FieldExpression expression) {
+		return addField(SECOND, expression);
+	}
 
-    @VisibleForTesting
-    CronBuilder addField(CronFieldName name, FieldExpression expression){
-        FieldConstraints constraints = definition.getFieldDefinition(name).getConstraints();
-        expression.accept(new ValidationFieldExpressionVisitor(constraints, definition.isStrictRanges()));
-        fields.put(name, new CronField(name, expression, constraints));
-        return this;
-    }
+	public Cron instance() {
+		return new Cron(definition, Lists.newArrayList(fields.values())).validate();
+	}
+
+	@VisibleForTesting
+	CronBuilder addField(CronFieldName name, FieldExpression expression) {
+		FieldConstraints constraints = definition.getFieldDefinition(name).getConstraints();
+		expression.accept(new ValidationFieldExpressionVisitor(constraints, definition.isStrictRanges()));
+		fields.put(name, new CronField(name, expression, constraints));
+
+		return this;
+	}
 }
