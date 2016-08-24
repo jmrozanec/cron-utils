a4b32b1597df75eef60da2a8ed31c9f5552a517f
Aurélien Manteaux <amanteaux@gmail.com>
2016-08-24 23:22:47 +0200

    codacy fix

--- src/main/java/com/cronutils/utils/Preconditions.java
+++ src/main/java/com/cronutils/utils/Preconditions.java
@@ -237,8 +237,8 @@ public class Preconditions {
    *     to strings using {@link String#valueOf(Object)}. Arguments can be null.
    */
   // Note that this is somewhat-improperly used from Verify.java as well.
-  static String format(String template, Object... args) {
-    template = String.valueOf(template); // null -> "null"
+  private static String format(String nullableTemplate, Object... args) {
+    String template = String.valueOf(nullableTemplate); // null -> "null"
 
     // start substituting the arguments into the '%s' placeholders
     StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
