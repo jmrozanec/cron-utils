10d2936a73e2699166ba74d32348dff28a69ff1e
Łukasz Bąk <lukaszbak01@gmail.com>
2016-09-13 02:07:48 +0200

    Failing unit test for issue #103

--- src/test/java/com/cronutils/utils/descriptor/CronDescriptorQuartzIntegrationTest.java
+++ src/test/java/com/cronutils/utils/descriptor/CronDescriptorQuartzIntegrationTest.java
@@ -10,18 +10,6 @@ import org.junit.Test;
 import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
-/*
- * Copyright 2015 jmrozanec
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 public class CronDescriptorQuartzIntegrationTest {
 
     private CronDescriptor descriptor;
@@ -106,6 +94,14 @@ public class CronDescriptorQuartzIntegrationTest {
     //TODO
     public void testEveryDayEveryFourHoursFromHour2() throws Exception {
         assertExpression("0 0 2/4 * * ?", "");
+    }
+    
+    /*
+     * Issue #103
+     */
+    @Test
+    public void testDescriptionDayOfWeek() {
+        assertExpression("* 0/1 * ? * TUE", "every minute at Tuesday day");
     }
 
     private void assertExpression(String cron, String description){
