0a9b80af0ae52527d2c40145ab2f481f36fc4184
Bob <meinc@web.de>
2017-05-25 11:57:35 +0200

    Revised Cron definitions to be closer to Quartz, if DoY is omitted. Added Javadoc.

--- src/test/java/com/cronutils/model/definition/TestCronDefinitionsFactory.java
+++ src/test/java/com/cronutils/model/definition/TestCronDefinitionsFactory.java
@@ -29,14 +29,85 @@ public class TestCronDefinitionsFactory {
                 .instance();
     }
 
+    /**
+     * Provides a <code>CronDefinition</code> that extends the Quartz Cron definition by an optional DoY field at the end.
+     <p>
+     * The cron expression is expected to be a string comprised of 6, 7 or 8
+     * fields separated by white space. Fields can contain any of the allowed
+     * values, along with various combinations of the allowed special characters
+     * for that field. The fields are as follows:
+     * <p>
+     * <table style="width:100%">
+     * <tr>
+     * <th>Field Name</th>
+     * <th>Mandatory</th>
+     * <th>Allowed Values</th>
+     * <th>Allowed Special Characters</th>
+     * </tr>
+     * <tr>
+     * <td>Seconds</td>
+     * <td>YES</td>
+     * <td>0-59</td>
+     * <td>* , - /</td>
+     * </tr>
+     * <tr>
+     * <td>Minutes</td>
+     * <td>YES</td>
+     * <td>0-59</td>
+     * <td>* , - /</td>
+     * </tr>
+     * <tr>
+     * <td>Hours</td>
+     * <td>YES</td>
+     * <td>0-23</td>
+     * <td>* , - /</td>
+     * </tr>
+     * <tr>
+     * <td>Day of month</td>
+     * <td>YES</td>
+     * <td>1-31</td>
+     * <td>* ? , - / L W</td>
+     * </tr>
+     * <tr>
+     * <td>Month</td>
+     * <td>YES</td>
+     * <td>1-12 or JAN-DEC</td>
+     * <td>* , -</td>
+     * </tr>
+     * <tr>
+     * <td>Day of week</td>
+     * <td>YES</td>
+     * <td>1-7 or SUN-SAT</td>
+     * <td>* ? , - / L #</td>
+     * </tr>
+     * <tr>
+     * <td>Year</td>
+     * <td>NO (if last field) YES (otherwise)</td>
+     * <td>empty, 1970-2099</td>
+     * <td>* , - /</td>
+     * </tr>
+     * <tr>
+     * <td>Day of year</td>
+     * <td>NO</td>
+     * <td>empty, 1-366</td>
+     * <td>* ? , - /</td>
+     * </tr>
+     * </table>
+     * <P>
+     * Thus in general cron expressions are as follows:
+     * <p>
+     * S M H DoM M DoW [Y [DoY]]
+     *
+     * @return the newly created <code>CronDefinition</code>.
+     */
     public static CronDefinition withDayOfYearDefinitionWhereYearAndDoYOptionals(){
         return CronDefinitionBuilder.defineCron()
                 .withSeconds().and()
                 .withMinutes().and()
                 .withHours().and()
-                .withDayOfMonth().supportsHash().supportsL().supportsW().supportsLW().supportsQuestionMark().and()
+                .withDayOfMonth().supportsL().supportsW().supportsLW().supportsQuestionMark().and()
                 .withMonth().and()
-                .withDayOfWeek().withValidRange(1, 7).withMondayDoWValue(2).supportsHash().supportsL().supportsW().supportsQuestionMark().and()
+                .withDayOfWeek().withValidRange(1, 7).withMondayDoWValue(2).supportsHash().supportsL().supportsQuestionMark().and()
                 .withYear().withValidRange(1970, 5000).optional().and()
                 .withDayOfYear().supportsQuestionMark().withValidRange(1, LEAP_YEAR_DAY_COUNT).optional().and()
                 .withCronValidation(CronConstraintsFactory.ensureEitherDayOfYearOrMonth())
