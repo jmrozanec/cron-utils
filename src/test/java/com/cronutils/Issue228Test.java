717006643ee30971dc12b3860f29b4717e623829
Jose Martin Rozanec <jmrozanec@gmail.com>
2017-11-29 23:35:48 -0300

    Bugfixes: add .isPresent() check on Optional<> statements.

--- src/test/java/com/cronutils/Issue228Test.java
+++ src/test/java/com/cronutils/Issue228Test.java
@@ -33,7 +33,8 @@ public class Issue228Test {
         // This is 9am on a day between the 1st and 7th which is a Monday (in this case it should be Oct 2
         Cron myCron = parser.parse("0 9 1-7 * 1");
         ZonedDateTime time = ZonedDateTime.parse("2017-09-29T14:46:01.166-07:00");
-        assertEquals(ZonedDateTime.parse("2017-10-02T09:00-07:00"), ExecutionTime.forCron(myCron).nextExecution(time).get());
+        ZonedDateTime next = ExecutionTime.forCron(myCron).nextExecution(time).isPresent()?ExecutionTime.forCron(myCron).nextExecution(time).get():null;
+        assertEquals(ZonedDateTime.parse("2017-10-02T09:00-07:00"), next);
     }
 
     @Test
@@ -43,7 +44,8 @@ public class Issue228Test {
         // This is 9am on Mon-Fri day between the 1st and 7th (in this case it should be Oct 2)
         Cron myCron = parser.parse("0 9 1-7 * 1-5");
         ZonedDateTime time = ZonedDateTime.parse("2017-09-29T14:46:01.166-07:00");
-        assertEquals(ZonedDateTime.parse("2017-10-02T09:00-07:00"), ExecutionTime.forCron(myCron).nextExecution(time).get());
+        ZonedDateTime next = ExecutionTime.forCron(myCron).nextExecution(time).isPresent()?ExecutionTime.forCron(myCron).nextExecution(time).get():null;
+        assertEquals(ZonedDateTime.parse("2017-10-02T09:00-07:00"), next);
     }
 
     @Test
@@ -53,7 +55,9 @@ public class Issue228Test {
         // This is 9am on Sat and Sun day between the 1st and 7th (in this case it should be Oct 1)
         Cron myCron = parser.parse("0 9 1-7 * 6-7");
         ZonedDateTime time = ZonedDateTime.parse("2017-09-29T14:46:01.166-07:00");
-        assertEquals(ZonedDateTime.parse("2017-10-01T09:00-07:00"), ExecutionTime.forCron(myCron).nextExecution(time).get());
+
+        ZonedDateTime next = ExecutionTime.forCron(myCron).nextExecution(time).isPresent()?ExecutionTime.forCron(myCron).nextExecution(time).get():null;
+        assertEquals(ZonedDateTime.parse("2017-10-01T09:00-07:00"), next);
     }
 
     @Test
@@ -63,7 +67,9 @@ public class Issue228Test {
         // This is 9am on Mon-Fri day between the 8th and 14th (in this case it should be Oct 9 Mon)
         Cron myCron = parser.parse("0 9 8-14 * 1-5");
         ZonedDateTime time = ZonedDateTime.parse("2017-09-29T14:46:01.166-07:00");
-        assertEquals(ZonedDateTime.parse("2017-10-09T09:00-07:00"), ExecutionTime.forCron(myCron).nextExecution(time).get());
+
+        ZonedDateTime next = ExecutionTime.forCron(myCron).nextExecution(time).isPresent()?ExecutionTime.forCron(myCron).nextExecution(time).get():null;
+        assertEquals(ZonedDateTime.parse("2017-10-09T09:00-07:00"), next);
     }
 
     @Test
@@ -73,6 +79,8 @@ public class Issue228Test {
         // This is 9am on Sat and Sun day between the 22nd and 28th (in this case it should be Oct 22)
         Cron myCron = parser.parse("0 9 22-28 * 6-7");
         ZonedDateTime time = ZonedDateTime.parse("2017-09-29T14:46:01.166-07:00");
-        assertEquals(ZonedDateTime.parse("2017-10-22T09:00-07:00"), ExecutionTime.forCron(myCron).nextExecution(time).get());
+
+        ZonedDateTime next = ExecutionTime.forCron(myCron).nextExecution(time).isPresent()?ExecutionTime.forCron(myCron).nextExecution(time).get():null;
+        assertEquals(ZonedDateTime.parse("2017-10-22T09:00-07:00"), next);
     }
 }
