2921a0930d1ac75a0e86ed272e61aa900f7d8aa4
Christoph <christoph.tonsing@gmail.com>
2018-08-22 09:34:32 +0200

    Issue 340: Added test for issue 340

---
+++ src/test/java/com/cronutils/Issue340Test.java
@@ -0,0 +1,34 @@
+package com.cronutils;
+
+import com.cronutils.model.Cron;
+import com.cronutils.model.definition.CronDefinition;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.model.time.ExecutionTime;
+import com.cronutils.parser.CronParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+import static com.cronutils.model.CronType.QUARTZ;
+
+public class Issue340Test {
+
+    @Test
+    // This test will enter a (bounded) infinite loop searching for last execution time
+    public void testGetTimeFromLastExecutionForScheduleWithDayOfWeekRangeCrossingLastDayOfWeekBoundary() {
+        String schedule = "0 0 * ? * MON-SUN *";
+        CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(QUARTZ);
+        CronParser parser = new CronParser(cronDefinition);
+        Cron quartzCron = parser.parse(schedule);
+
+        ZonedDateTime time = ZonedDateTime.now();
+        ExecutionTime executionTime = ExecutionTime.forCron(quartzCron);
+
+        // Time from last execution to now
+        Duration timeFromLastExecution = executionTime.timeFromLastExecution(time).get();
+        Assert.assertTrue(timeFromLastExecution.toMinutes() <= 60 );
+    }
+
+}
