f8d1fd3e521f48195aff118c452550980f34197a
Alex Pang <pangyikhei+github@gmail.com>
2018-10-20 18:12:04 -0400

    #340 - Handle day of week rollover

--- src/test/java/com/cronutils/Issue340Test.java
+++ src/test/java/com/cronutils/Issue340Test.java
@@ -6,18 +6,23 @@ import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.model.time.ExecutionTime;
 import com.cronutils.parser.CronParser;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.internal.util.collections.Sets;
 
 import java.time.Duration;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.cronutils.model.CronType.QUARTZ;
-@Ignore
+
 public class Issue340Test {
 
     @Test
-    // This test will enter a (bounded) infinite loop searching for last execution time
     public void testGetTimeFromLastExecutionForScheduleWithDayOfWeekRangeCrossingLastDayOfWeekBoundary() {
         String schedule = "0 0 * ? * MON-SUN *";
         CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(QUARTZ);
@@ -28,8 +33,35 @@ public class Issue340Test {
         ExecutionTime executionTime = ExecutionTime.forCron(quartzCron);
 
         // Time from last execution to now
-        Duration timeFromLastExecution = executionTime.timeFromLastExecution(time).get();
-        Assert.assertTrue(timeFromLastExecution.toMinutes() <= 60 );
+        Optional<Duration> timeFromLastExecution = executionTime.timeFromLastExecution(time);
+        Assert.assertTrue(timeFromLastExecution.isPresent());
+        Assert.assertTrue(timeFromLastExecution.get().toMinutes() <= 60);
+    }
+
+    @Test
+    public void testDayOfWeekRollover() {
+        // Every Friday to Tuesday (Fri, Sat, Sun, Mon, Tue) at 5 AM
+        String schedule = "0 0 5 ? * FRI-TUE *";
+        // Java DayOfWeek is MON (1) to SUN (7)
+        Set<Integer> validDaysOfWeek = Sets.newSet(1, 2, 5, 6, 7);
+        CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(QUARTZ);
+        CronParser parser = new CronParser(cronDefinition);
+        Cron quartzCron = parser.parse(schedule);
+
+        ZonedDateTime time = ZonedDateTime.now();
+        ExecutionTime executionTime = ExecutionTime.forCron(quartzCron);
+
+        // Check the next 100 execution times
+        for (int i = 0; i < 100; i++) {
+            Optional<ZonedDateTime> nextExecution = executionTime.nextExecution(time);
+            Assert.assertTrue(nextExecution.isPresent());
+            time = nextExecution.get();
+            int dayOfWeek = time.getDayOfWeek().getValue();
+            Assert.assertTrue(validDaysOfWeek.contains(dayOfWeek));
+            Assert.assertEquals(5, time.getHour());
+            Assert.assertEquals(0, time.getMinute());
+            Assert.assertEquals(0, time.getSecond());
+        }
     }
 
 }
