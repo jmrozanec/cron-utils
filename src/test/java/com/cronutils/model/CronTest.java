fb9fb74a2e2a11d7d6e7e91e068f02facf33bc65
Joze Rozanec <jmrozanec@gmail.com>
2018-11-24 19:13:21 +0100

    Extract CronUtils to another repository, so that ScheduledExpression dependency is not required.

--- src/test/java/com/cronutils/model/CronTest.java
+++ src/test/java/com/cronutils/model/CronTest.java
@@ -13,7 +13,6 @@
 
 package com.cronutils.model;
 
-import javax.ejb.ScheduleExpression;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -32,12 +31,10 @@ import com.cronutils.descriptor.CronDescriptor;
 import com.cronutils.mapper.CronMapper;
 import com.cronutils.model.definition.CronDefinition;
 import com.cronutils.model.definition.CronDefinitionBuilder;
-import com.cronutils.model.definition.TestCronDefinitionsFactory;
 import com.cronutils.model.field.CronField;
 import com.cronutils.model.field.CronFieldName;
 import com.cronutils.model.field.expression.FieldExpression;
 import com.cronutils.parser.CronParser;
-import com.cronutils.utils.CronUtils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -149,39 +146,6 @@ public class CronTest {
                 assertEquals(expected.asString(), actual.asString());
             }
         }
-    }
-
-    @Test
-    public void testAsScheduleExpression(){
-        final CronDefinition cronDefinition = TestCronDefinitionsFactory.quartzNoDoWAndDoMRestrictionBothSameTime();
-        final CronParser cronParser = new CronParser(cronDefinition);
-        final Cron cron = cronParser.parse("0 * * 1 * MON *");
-        ScheduleExpression expression = CronUtils.asScheduleExpression(cron);
-
-        assertNotNull(expression);
-        assertEquals(cron.retrieve(CronFieldName.SECOND).getExpression().asString(), expression.getSecond());
-        assertEquals(cron.retrieve(CronFieldName.MINUTE).getExpression().asString(), expression.getMinute());
-        assertEquals(cron.retrieve(CronFieldName.HOUR).getExpression().asString(), expression.getHour());
-        assertEquals(cron.retrieve(CronFieldName.DAY_OF_MONTH).getExpression().asString(), expression.getDayOfMonth());
-        assertEquals(cron.retrieve(CronFieldName.MONTH).getExpression().asString(), expression.getMonth());
-        assertEquals(cron.retrieve(CronFieldName.DAY_OF_WEEK).getExpression().asString(), expression.getDayOfWeek());
-        assertEquals(cron.retrieve(CronFieldName.YEAR).getExpression().asString(), expression.getYear());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testAsScheduleExpressionQuestionMarkFails() {
-        final CronDefinition quartzcd = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ);
-        final CronParser quartz = new CronParser(quartzcd);
-        final Cron cron = quartz.parse("0 * * ? * MON *");
-        CronUtils.asScheduleExpression(cron);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testAsScheduleExpressionDoYNotSupported() {
-        final CronDefinition cronDefinition = TestCronDefinitionsFactory.withDayOfYearDefinitionWhereNoQuestionMarkSupported();
-        final CronParser cronParser= new CronParser(cronDefinition);
-        final Cron cron = cronParser.parse("0 0 0 1 1-3 * * 1/14");
-        CronUtils.asScheduleExpression(cron);
     }
 
     //@Test TODO
