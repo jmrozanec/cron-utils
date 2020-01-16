9c92f2d4bf0866760e8ee834d298e1d21ace77ed
Kellen Donohue <kdonohue@palantir.com>
2020-01-16 14:04:07 -0800

    simplify and fix the test

--- src/test/java/com/cronutils/Issue413Test.java
+++ src/test/java/com/cronutils/Issue413Test.java
@@ -1,6 +1,5 @@
 package com.cronutils;
 
-import com.cronutils.model.Cron;
 import com.cronutils.model.CronType;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.parser.CronParser;
@@ -14,13 +13,12 @@ public class Issue413Test {
     @Test
     public void testFridayToSaturdayQuartz() {
         final CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.UNIX));
-        final Cron quartzCron = parser.parse("* * * */12 *");
         try {
-            quartzCron.validate();
+            parser.parse("* * * */12 *");
             // expected to fail.
             fail();
         } catch (IllegalArgumentException expected) {
-            assertEquals("Failed to parse '* * * */12 *'. Period 12 not in range (1, 12]", expected.getMessage());
+            assertEquals("Failed to parse '* * * */12 *'. Period 12 not in range [1, 12)", expected.getMessage());
         }
     }
 }
