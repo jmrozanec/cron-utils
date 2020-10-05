00e1e197d89d779c516b0650dff220c164a6326a
albertotn <2526457+albertotn@users.noreply.github.com>
2020-10-05 23:18:51 +0200

    #440 fixed error for issue #440

---
+++ src/test/java/com/cronutils/Issue440Test.java
@@ -0,0 +1,30 @@
+package com.cronutils;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cronutils.descriptor.CronDescriptor;
+import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.parser.CronParser;
+
+public class Issue440Test {
+
+	private CronParser parser;
+
+	@Before
+	public void setUp() {
+		parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ));
+	}
+
+	@Test
+	public void testCase1() {
+		CronDescriptor descriptor = CronDescriptor.instance(Locale.UK);
+		String description = descriptor.describe(parser.parse("* 2,1/31 * * * ?"));
+		assertTrue(description.equalsIgnoreCase("at 2 minutes every 31 minutes"));
+	}
+}
