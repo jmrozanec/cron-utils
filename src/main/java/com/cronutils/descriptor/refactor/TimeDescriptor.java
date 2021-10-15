9fcb6e4178ed2d971db99aab60dd7b07db6dda83
albertotn <2526457+albertotn@users.noreply.github.com>
2021-10-15 09:12:35 +0200

    fixed some tests in italian

--- src/main/java/com/cronutils/descriptor/refactor/TimeDescriptor.java
+++ src/main/java/com/cronutils/descriptor/refactor/TimeDescriptor.java
@@ -1,5 +1,13 @@
 package com.cronutils.descriptor.refactor;
 
+import java.text.ChoiceFormat;
+import java.text.Format;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
 import com.cronutils.model.Cron;
 import com.cronutils.model.field.CronField;
 import com.cronutils.model.field.CronFieldName;
@@ -9,80 +17,66 @@ import com.cronutils.model.field.expression.On;
 import com.cronutils.utils.Preconditions;
 import com.cronutils.utils.StringUtils;
 
-import java.text.ChoiceFormat;
-import java.text.Format;
-import java.text.MessageFormat;
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.Map;
-import java.util.ResourceBundle;
-
 public class TimeDescriptor {
 
-    private final ResourceBundle resourceBundle;
+	private final ResourceBundle resourceBundle;
 
-    public TimeDescriptor(final ResourceBundle resourceBundle) {
-        this.resourceBundle = Preconditions.checkNotNull(resourceBundle, "The resource bundle must not be null");
-    }
+	public TimeDescriptor(final ResourceBundle resourceBundle) {
+		this.resourceBundle = Preconditions.checkNotNull(resourceBundle, "The resource bundle must not be null");
+	}
 
+	public String describe(final Cron cron) {
+		return describe(cron.retrieveFieldsAsMap());
+	}
 
-    public String describe(final Cron cron) {
-        return describe(cron.retrieveFieldsAsMap());
-    }
+	private String describe(final Map<CronFieldName, CronField> expressions) {
 
-    private String describe(final Map<CronFieldName, CronField> expressions) {
+		if (expressions.containsKey(CronFieldName.SECOND)) {
+			final CronField cronField = expressions.get(CronFieldName.SECOND);
+			if (cronField.getExpression() instanceof Always) {
+				return describeEverySecond(1);
+			} else if (cronField.getExpression() instanceof On) {
+				return describeAtSecond(((On) cronField.getExpression()).getTime().getValue());
+			} else if (cronField.getExpression() instanceof Every) {
+				return describeEverySecond(((Every) cronField.getExpression()).getPeriod().getValue());
+			}
+		}
 
-        if (expressions.containsKey(CronFieldName.SECOND)) {
-            final CronField cronField = expressions.get(CronFieldName.SECOND);
-            if (cronField.getExpression() instanceof Always) {
-                return describeEverySecond(1);
-            } else if (cronField.getExpression() instanceof On) {
-                return describeAtSecond(((On)cronField.getExpression()).getTime().getValue());
-            } else if (cronField.getExpression() instanceof Every) {
-                return describeEverySecond(((Every)cronField.getExpression()).getPeriod().getValue());
-            }
-        }
+		return StringUtils.EMPTY;
+	}
 
+	private String describeEverySecond(final int second) {
+		final double[] secondsLimit = { 1, 2 };
+		final String[] secondsStrings = { resourceBundle.getString("oneSecond"),
+				resourceBundle.getString("multipleSeconds") };
+		final double[] everyLimit = { 1, 2 };
+		final String[] everyStrings = { resourceBundle.getString("every_one"),
+				resourceBundle.getString("every_multi") };
 
-        return StringUtils.EMPTY;
-    }
+		final ChoiceFormat secondsChoiceFormat = new ChoiceFormat(secondsLimit, secondsStrings);
+		final ChoiceFormat everyChoiceFormat = new ChoiceFormat(everyLimit, everyStrings);
+		final String pattern = resourceBundle.getString("pattern_every_seconds");
 
-    private String describeEverySecond(final int second) {
-        final double[] secondsLimit = {1, 2};
-        final String[] secondsStrings = {
-                resourceBundle.getString("oneSecond"),
-                resourceBundle.getString("multipleSeconds")
-        };
-        final double[] everyLimit = {1,2};
-        final String[] everyStrings = {
-                resourceBundle.getString("every_one"),
-                resourceBundle.getString("every_multi")
-        };
+		final MessageFormat messageFormat = new MessageFormat(pattern, Locale.UK);
 
-        final ChoiceFormat secondsChoiceFormat = new ChoiceFormat(secondsLimit, secondsStrings);
-        final ChoiceFormat everyChoiceFormat = new ChoiceFormat(everyLimit, everyStrings);
-        final String pattern = resourceBundle.getString("pattern_every_seconds");
+		final Format[] formats = { everyChoiceFormat, secondsChoiceFormat, NumberFormat.getInstance() };
+		messageFormat.setFormats(formats);
+		final Object[] messageArguments = { second, second, second };
+		final String result = messageFormat.format(messageArguments);
+		return result;
+	}
 
-        final MessageFormat messageFormat = new MessageFormat(pattern, Locale.UK);
+	private String describeAtSecond(final int second) {
 
-        final Format[] formats = { everyChoiceFormat, secondsChoiceFormat, NumberFormat.getInstance() };
-        messageFormat.setFormats(formats);
-        final Object[] messageArguments = {second, second, second };
-        final String result = messageFormat.format(messageArguments);
-        return result;
-    }
+		final String pattern = resourceBundle.getString("pattern_at_second");
 
-    private String describeAtSecond(final int second) {
+		final MessageFormat messageFormat = new MessageFormat(pattern, Locale.UK);
+		final Format[] formats = { NumberFormat.getInstance() };
+		messageFormat.setFormats(formats);
 
-        final String pattern = resourceBundle.getString("pattern_at_second");
-
-        final MessageFormat messageFormat = new MessageFormat(pattern, Locale.UK);
-        final Format[] formats = {NumberFormat.getInstance()};
-        messageFormat.setFormats(formats);
-
-        final Object[] messageArguments = { second };
-        final String result = messageFormat.format(messageArguments);
-        return result;
-    }
+		final Object[] messageArguments = { second };
+		final String result = messageFormat.format(messageArguments);
+		return result;
+	}
 
 }
